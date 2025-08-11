e959608774894a03d291c073d0e9ff7dcbac8315
thevoid12 <iamvinod2001@gmail.com>
2025-08-11 09:48:30 +0530

    dava driven react using prop and  array of jsx element

--- data-driven-react-2/src/App.jsx
+++ data-driven-react-2/src/App.jsx
@@ -1,23 +1,30 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
 import Entry from './components/Entry.jsx'
+import Data from './data.js'
 import './App.css'
 
 function App() {
+  // data contains array of js object which we need to convert into array of jsx Array element [<Entry sgdfsdfg/>,<Entry sgfsdgf/>,<Entry fdgdf/>,<Entry dfgjsdh/>]
+  const Datajsx= Data.map((item)=>{
+    return <Entry 
+    key={item.id} // unique value which we need to pass on to the prop for react to uniquely identify 
+    img={{
+      src:item.img.src,
+      alt:item.img.alt
+    }}
+    title={item.title}
+    country={item.country}
+    googleMapsLink={item.googleMapsLink}
+    dates={item.dates}
+    text={item.text}
+
+  />
+  })
   return (
     <>
     <Header />
-    <Entry 
-    img={{
-      src: "https://scrimba.com/links/travel-journal-japan-image-url",
-      alt:"Mount Fuji"
-    }}
-    title="Mount Fuji"
-    country="japan"
-    googleMapsLink ="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-    dates="12 Jan, 2021 - 24 Jan, 2021"
-    text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-    />
+    {Datajsx}
     </>
   )
 }
